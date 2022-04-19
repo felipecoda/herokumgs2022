0e5e467cd4a7da8fb574a6527a45a2c139f6903e
felipecoda <101847915+felipecoda@users.noreply.github.com>
2022-04-18 22:36:05 -0700

    make it better

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
     mode: 'development',
     entry: {
-        dabundle: path.resolve(__dirname, 'src/index.mjs'),
+        dabundle: path.resolve(__dirname, 'src/index.js'),
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
